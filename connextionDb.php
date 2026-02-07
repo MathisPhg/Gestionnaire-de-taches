8f421765b95f0ecefc0ff6aaaea0eb56bcfe3a99
MathisPhg <mathis.phg@gmail.com>
2026-02-07 17:17:05 +0000

    feat(index.html): ajout du responsive

--- connextionDb.php
+++ connextionDb.php
@@ -1,7 +1,9 @@
 <?php
 
-$db = new PDO(
-    'mysql:host=db.3wa.io;port=3306;dbname=mathisphuong_todolist;charset=utf8',
-    'mathisphuong',
-    '81efb03e36b8ef8a7a00bf159d5bd77e'
-);
+// Connexion à la base de données
+function connectToDB() {
+    $dsn = "mysql:host=localhost;dbname=todolist";
+    $db = new PDO($dsn, "root", "");
+    return $db;
+}
+
