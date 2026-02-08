d88279cd77073ad701622b8ffaeccd6adf162570
MathisPhg <mathis.phg@gmail.com>
2026-02-08 22:37:52 +0000

    fix(ranking): ajoute un message quand il n'y a encore de classement

--- connectToDB.php
+++ connectToDB.php
@@ -2,22 +2,10 @@
 
 // Connexion à la base de données
 function connectToDB() {
-    $dsn = "mysql:host=db.3wa.io;port=3306;dbname=mathisphuong_todolist;charset=utf8";
-    $User = "mathisphuong";
-    $Password = "81efb03e36b8ef8a7a00bf159d5bd77e";
+    $dsn = "mysql:host=localhost;dbname=todolist";
+    $User = "root";
+    $Password = "";
     $db = new PDO($dsn, $User, $Password);
     return $db;
 }
-
-
-
-/*
-
-$db = new PDO(
-    'mysql:host=db.3wa.io;port=3306;dbname=mathisphuong_todolist;charset=utf8',
-    'mathisphuong',
-    '81efb03e36b8ef8a7a00bf159d5bd77e'
-);
-
-*/
 
